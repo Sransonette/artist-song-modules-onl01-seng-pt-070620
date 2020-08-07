4ada09cf0057727d2e7b5af3fad81c2339e393cc
Sean Ransonette <sransonette@gmail.com>
2020-08-07 21:20:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   extend Findable
   include Paramable
   extend Memorable::ClassMethods
-include Memorable::InstanceMethods
+  include Memorable::InstanceMethods
   
 
   attr_accessor :name
